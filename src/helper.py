17a1f5ae940dae85f1d834b2e0207bd3564d8c10
Fernando <hli@fqm.ca>
2025-12-04 17:20:40 -0500

    finish the bot

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,9 @@
-from langchain.document_loaders import PyMuPDFLoader,DirectoryLoader
+from langchain_community.document_loaders import PyMuPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.prompts import PromptTemplate
+from typing import List
+from langchain.schema import Document
+
 def extract_data_pdf(path):
     loader=DirectoryLoader(path,glob="*.pdf",loader_cls=PyMuPDFLoader)
     documents=loader.load()
@@ -12,6 +15,17 @@ def  text_split(pdf_data):
     return texts   
 
 def download_hugging_face_embeddings(model_name="sentence-transformers/all-MiniLM-L6-v2"):
-    from langchain.embeddings import HuggingFaceEmbeddings
+    from langchain_huggingface import HuggingFaceEmbeddings
     embeddings=HuggingFaceEmbeddings(model_name=model_name)
     return embeddings
+
+def filter_to_minimal_docs(docs:List[Document])->List[Document]:
+    """
+    Given a list of Document objects, return a new list of Document objects
+    containing only 'source' in metadata and the original page_content.
+    """
+    minimal_docs: List[Document]=[]
+    for doc in docs:
+        src=doc.metadata.get('source','')
+        minimal_docs.append(Document(page_content=doc.page_content,metadata={'source':src}))
+    return minimal_docs
--- src/prompt.py
+++ src/prompt.py
@@ -10,3 +10,13 @@ Question: {question}
 Only return the helpful answer below and nothing else.
 Helpful answer:
 """
+
+openai_template= (
+    "You are an Medical assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
